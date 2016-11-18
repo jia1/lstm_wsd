aa95d6a93dfeb2887aed25ecea03369e0c1bb0f4
kageback <kageback@chalmers.se>
2016-11-18 01:23:14 +0100

    fixed the chained commands for the docker command

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 docker build -t kageback/wsd . && \
-docker run -t --rm kageback/wsd python -u train.py && \
-				 python -u eval.py && \
-				 python -u score.py ./tmp/result ./data/senseval2/Senseval2.key ./data/senseval2/sensemap
+docker run -t --rm kageback/wsd /bin/bash -c "python -u train.py;
+					      python -u eval.py;
+				              python -u score.py ./tmp/result ./data/senseval2/Senseval2.key ./data/senseval2/sensemap"
+
 
