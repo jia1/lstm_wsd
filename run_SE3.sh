b34edf6e2fb8a05d66e8d93c1a3711e41182d816
Mikael Kågebäck <kageback@chalmers.se>
2016-11-20 19:49:19 +0100

    Forgot to add this to prev commit

--- run_SE3.sh
+++ run_SE3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker build -t kageback/wsd . && \
-docker run -t --rm kageback/wsd /bin/bash -c "python -u train.py;
-					      python -u eval.py;
+docker run -t --rm kageback/wsd /bin/bash -c "python -u train.py 3;
+					      python -u eval.py 3;
 				              python -u score.py ./tmp/result ./data/senseval3/EnglishLS.test.key ./data/senseval3/EnglishLS.sensemap"
 
 
